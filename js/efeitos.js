3738362c213e0584c1da458bcb7060a08e3db49e
Abilio Castro <castro.abilio@gmail.com>
2019-04-24 12:16:55 -0300

    Começando responsividade

--- js/efeitos.js
+++ js/efeitos.js
@@ -2,3 +2,7 @@ $("#ver-mais").click(function() {
     $(".escondido").css("display", "inline-block");
     $("#ver-mais").css("display", "none");
 });
+
+$("#menu-butao").click(function() {
+    $("#menu").toggle();
+});
